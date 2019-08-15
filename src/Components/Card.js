506005b4424c17fea3fb2a55e9ddd544933a1a45
milano12345 <dmilano857@gmail.com>
2019-08-15 14:45:37 -0500

    added card component from semantic-ui

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,37 @@
-import React from 'react';
+function Card(props) {
+  return <div>
+       <Intro>Welcome Travelers!</Intro>
+      <Pic src={props.data.url} alt="nasa image of the day"/>
+      <Title>{props.data.title}</Title>
+      <Text>{props.data.explanation}</Text>
+      <Text>{props.data.date}</Text>
+      <Text>{props.data.copyright}</Text>
+  </div>
 
-const Card = (props) => {
-return <div>
-    <div>{props.data.date}</div>
-    <div>{props.data.explanation}</div>
-    <div>{props.data.url}</div>
-</div>
 }
 
-export default Card;
+import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
+
+const CardExampleCard = () => (
+  <Card>
+    <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Welcome Travelers!</Card.Header>
+      <Card.Meta>
+        <span className='date'>{props.data.date}</span>
+      </Card.Meta>
+      <Card.Description>
+        Matthew is a musician living in Nashville.
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends
+      </a>
+    </Card.Content>
+  </Card>
+)
+
+export default CardExampleCard
