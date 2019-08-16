cf014ff524996a4dc003ef57c74286d013b5ed02
milano12345 <dmilano857@gmail.com>
2019-08-15 19:09:34 -0500

    daniel-milano

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,12 @@
-
 import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
+import {TweenMax} from 'gsap'
 
+function handleChange() {
+  TweenMax.to('.rocket1', 4, {top:-1200, delay:0});
+  TweenMax.to('.rocket2', 4, {top:-1200, delay:1});
+}
 function CreateCard(props) {
-
-
  return <Card>
     <Image src={props.data.url} alt= 'nasa photo of the day' wrapped ui={false} />
     <Card.Content>
@@ -22,6 +24,9 @@ function CreateCard(props) {
         {props.data.copyright}
       </a>
     </Card.Content>
+    <Button onClick={handleChange}>🚀🚀Click Here🚀🚀</Button>
+    <img className='rocket1' src='https://www.searchpng.com/wp-content/uploads/2019/01/Rocket-Ship-Png-715x715.png' alt='rocketship'/>
+    <img className='rocket2' src='https://www.searchpng.com/wp-content/uploads/2019/01/Rocket-Ship-Png-715x715.png' alt='rocketship'/>
   </Card>
 }
 
